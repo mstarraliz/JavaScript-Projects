7fd46ff9f3776196903ac6e7a0f73419a259feba
mstarraliz <65620198+mstarraliz@users.noreply.github.com>
2020-08-03 15:23:41 -0700

    JavaScript Math
    Various math operations

---
+++ Basic_JavaScript_Projects/JS/Basic_JavaScript_3.js
@@ -0,0 +1,36 @@
+
+function addition_Function() {
+    var addition = 2 + 2;
+    document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
+}
+
+function subtraction_Function() {
+    var Subtraction = 5 - 2;
+    document.getElementById("Math2").innerHTML = "5 - 2 = " + Subtraction;
+}
+
+function multiplication_Function() {
+    var Multiplication = 6 * 8;
+    document.getElementById("Math3").innerHTML = "6 x 8 = " + Multiplication;
+}
+
+function division_Function() {
+    var Division = 48 / 6;
+    document.getElementById("Math4").innerHTML = "48 / 6 = " + Division;
+}
+
+function more_Math() {
+    var simple_Math = (1 + 2) * 10 / 2 - 5;
+    document.getElementById("Math5").innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals = " + simple_Math;
+}
+
+function modulus_Operator() {
+    var simple_Math2 = 25 % 6;
+    document.getElementById("Math6").innerHTML = "When you divide 25 by 6 you have a remainder of: " + simple_Math2;
+}
+
+function negation_Operator() {
+    var x = 10;
+    document.getElementById("Math7").innerHTML = -x;
+}
+
